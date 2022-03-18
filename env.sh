d8e3f1e4b1a5c0c423fd1c5d83b1f83ef066ae97
dANW34V3R <dan.weaver@hotmail.co.uk>
2022-03-18 12:13:15 +0000

    Submission

--- env.sh
+++ env.sh
@@ -1,2 +1,7 @@
 # Add any `module load` or `export` commands that your code needs to
 # compile and run to this file.
+module load languages/gcc/9.3.0
+module load languages/intel/2020-u4
+export OMP_NUM_THREADS=28 
+export OMP_PROC_BIND=true 
+export OMP_PLACES=cores
